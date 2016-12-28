9df6db2f1d3af1235b5c85b7dad9900e41a3af01
Nathan Phillip Brink <binki@gentoo.org>
2016-12-28 06:34:15 +0000

    Ensure tests fail correctly.

--- test/example.test.js
+++ test/example.test.js
@@ -2,19 +2,34 @@
 
 const assert = require('assert');
 const crossSpawn = require('cross-spawn');
+const fs = require('fs');
 const path = require('path');
 
 describe('example', function () {
   this.timeout(20000);
 
   it('should succesfully build a bundle', function (done) {
-    crossSpawn('webpack', {
-      cwd: path.join(__dirname, '..', 'examples', 'simple'),
+    const simpleDir = path.join(__dirname, '..', 'examples', 'simple');
+    const bundleJsPath = path.join(simpleDir, 'bundle.js');
+    let exists = false;
+    try {
+      fs.accessSync(bundleJsPath);
+      exists = true;
+    } catch (ex) {
+    }
+    if (exists) {
+      fs.unlinkSync(bundleJsPath);
+    }
+    crossSpawn('webpack', ['--bail', ], {
+      cwd: simpleDir,
       stdio: 'inherit',
     })
-      .on('error', ex => done(ex))
+      .on('error', done)
       .on('exit', code => {
+          console.log(`code=${code}`);
         assert.equal(code, 0);
+          fs.accessSync(bundleJsPath);
+          console.log(`being done`);
         done();
       })
     ;
