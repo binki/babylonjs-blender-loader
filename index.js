ddbbe064264fffae3e7d88d9612ee796647b79ca
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2017-01-01 00:07:03 -0500

    Use tmp-promise instead of webpack-util-cleantmp.
    Fixes #2 because webpack-util-cleantmp relies on APIs not available
    until node-5.

--- index.js
+++ index.js
@@ -4,29 +4,14 @@ const pool = require('./workerPool.js');
 
 const promisify = require('promisify-node');
 const fs = promisify('fs');
-
-// cleantmp(actionOptionallyReturningPromise).then(…)
-const cleantmp = (() => {
-  const cleantmp = require('webpack-util-cleantmp').cleantmp;
-  return action => new Promise((resolve, reject) => {
-    const subscription = cleantmp().subscribe(dir => {
-      const p = Promise.resolve().then(() => action(dir))
-      // Useful for debugging—keep the tempdir around for long enough to figure out what’s happening.
-      //.then(() => new Promise((resolve, reject) => setTimeout(resolve, 12000)))
-      ;
-      p.catch().then(() => subscription.unsubscribe());
-      p.then(resolve, reject);
-    });
-  });
-})();
-
 const path = require('path');
+const tmp = require('tmp-promise');
 
 module.exports = function (source) {
   this.cacheable();
   const callback = this.async();
-  cleantmp(dir => {
-    const input = path.join(dir, 'input.blend');
+  tmp.dir().then(dirInfo => {
+    const input = path.join(dirInfo.path, 'input.blend');
     return fs.writeFile(input, source).then(() => {
       return pool.acquire().then(worker => {
         return worker.process(input).then(job => {
